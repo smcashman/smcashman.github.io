4726e5755e698dd282495160ccd6280b975da195
Sara Cashman <saramcashman@gmail.com>
2018-09-25 20:08:57 -0400

    add text rotate remove form

--- js/custom.js
+++ js/custom.js
@@ -49,6 +49,19 @@ $(document).ready(function() {
 		}
 	});
 
+	//textmorph
+	$("#js-rotating").Morphext({
+    // The [in] animation type. Refer to Animate.css for a list of available animations.
+    animation: "bounceIn",
+    // An array of phrases to rotate are created based on this separator. Change it if you wish to separate the phrases differently (e.g. So Simple | Very Doge | Much Wow | Such Cool).
+    separator: ",",
+    // The delay between the changing of each phrase in milliseconds.
+    speed: 2500,
+    complete: function () {
+        // Called after the entrance animation is executed.
+    }
+		});
+
 	// Text Rotator
 	$('.rotate').each(function(){
 		var el = $(this);
